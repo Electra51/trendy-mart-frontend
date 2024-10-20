698423436d3bd45d671648fdb71f1924ae5bec41
safayet nur <safayet@merchantbay.com>
2024-10-21 01:03:36 +0600

    hero section with responsive

--- src/Components/HomePageComponent/Hero/HeroSection.js
+++ src/Components/HomePageComponent/Hero/HeroSection.js
@@ -1,75 +1,11 @@
 import React from "react";
 import "./hero.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
-import img1 from "../../../assets/1.png";
-import img2 from "../../../assets/2.png";
-import img3 from "../../../assets/3.png";
-import img5 from "../../../assets/4.png";
-import Button from "../../Common/Button/Button";
-
+import heroImg from "../../../assets/banner1.jpg";
 const HeroSection = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
   return (
-    <Slider {...settings} className="slider-container">
-      <div className="hero-section">
-        <img src={img2} alt="" />
-      </div>
-      <div className="hero-section">
-        <img src={img3} alt="" />
-      </div>
-      <div className="hero-section">
-        <img src={img5} alt="" />
-      </div>{" "}
-      <div className="hero-section">
-        <div className="hero-content">
-          <p>Our Exclusive</p>
-          <h1 className="hero-title">ReadStarts</h1>
-
-          <p className="hero-para">
-            {" "}
-            We have all your needs! Are you looking for the best performance
-          </p>
-          <Button title={"Buy Now"} color="#F08F33" />
-        </div>
-        <img src={img1} alt="" />
-      </div>
-    </Slider>
+    <div className="hero-section">
+      <img src={heroImg} alt="" />
+    </div>
   );
 };
 
